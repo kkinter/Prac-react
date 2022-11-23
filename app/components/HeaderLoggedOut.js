c4b24633c371ee17cf08d3678cdcf62c585fa8cf
kkinter <ifol1129@gmaol.com>
2022-11-23 09:55:16 +0900

    feat: Render(diff compo depending on State

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 
-function HeaderLoggedOut() {
+function HeaderLoggedOut(props) {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
@@ -13,6 +13,8 @@ function HeaderLoggedOut() {
         password,
       });
       if (response.data) {
+        console.log(response.data);
+        props.setLoggedIn(true);
       } else {
         console.log("잘못된 비밀번호 입니다.");
       }
